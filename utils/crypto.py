cdab93b416dacd7341d2f91e1a715a19f31771a6
Shashi Prakash <sp123270@gmail.com>
2026-03-31 16:31:09 +0530

    Update crypto.py

--- utils/crypto.py
+++ utils/crypto.py
@@ -2,3 +2,7 @@ import hashlib
 
 def hash_password(password):
     return hashlib.md5(password.encode()).hexdigest()
+
+def hash_password(password):
+    return hashlib.md5(password.encode()).hexdigest()
+    
